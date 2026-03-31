cb0cd9ae44739ec9458288dd3399dfc66d4647cb
Asteria Moon <asteriasmoons@outlook.com>
2026-03-30 22:56:06 -0500

    standalone

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const withMDX = createMDX({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-  output: "export",
+  output: "standalone",
 };
 
 export default withMDX(nextConfig);
