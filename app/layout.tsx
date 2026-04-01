2364ac63135ce0cbb21d0f7f4b0fe64d0564000b
Asteria Moon <asteriasmoons@outlook.com>
2026-04-01 08:34:02 -0500

    new favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Elysium Docs",
   description: "Docs site",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({
